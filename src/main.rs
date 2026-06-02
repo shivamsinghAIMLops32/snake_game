baac32f2acec4a4d8b8c3319d457aeeadc896c3c
Shivam Singh <136737397+shivamsinghAIMLops32@users.noreply.github.com>
2026-06-02 18:22:51 +0530

    basic movements are done and updates

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,39 @@
+mod player;
+
 use graphics::{clear, rectangle};
 use piston_window::*;
+use player::{Direction, Player};
 
-mod player;
-use player::Player;
 fn main() {
-    let player = Player::new();
+    let mut player = Player::new();
 
-println!("{} {}", player.x, player.y);
-    let mut window: PistonWindow = WindowSettings::new(
-        "Snake Game",
-        [600, 600]
-    )
-    .exit_on_esc(true)
-    .build()
-    .unwrap();
+    let mut window: PistonWindow = WindowSettings::new("Snake Game", [600, 600])
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
 
     while let Some(event) = window.next() {
+        // Game update
+        if let Some(Button::Keyboard(key)) = event.press_args() {
+            match key {
+                Key::Up => player.set_direction(Direction::Up),
+                Key::Down => player.set_direction(Direction::Down),
+                Key::Left => player.set_direction(Direction::Left),
+                Key::Right => player.set_direction(Direction::Right),
+                _ => {}
+            }
+        }
+        if event.update_args().is_some() {
+            player.update();
+        }
+
+        // Draw
         window.draw_2d(&event, |c, g, _| {
             clear([0.0, 0.0, 0.0, 1.0], g);
 
             rectangle(
-                [0.0, 1.0, 0.0, 1.0], // green
-                [100.0, 100.0, 50.0, 50.0], // x,y,w,h
+                [0.0, 1.0, 0.0, 1.0],
+                [player.x, player.y, 50.0, 50.0],
                 c.transform,
                 g,
             );
--- src/player.rs
+++ src/player.rs
@@ -1,6 +1,14 @@
+pub enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
 pub struct Player {
     pub x: f64,
     pub y: f64,
+    pub direction: Direction,
 }
 
 impl Player {
@@ -8,6 +16,24 @@ impl Player {
         Self {
             x: 100.0,
             y: 100.0,
+            direction: Direction::Right,
         }
+    }
+
+    pub fn update(&mut self) {
+        match self.direction {
+            Direction::Up => self.y -= 5.0,
+            Direction::Down => self.y += 5.0,
+            Direction::Left => self.x -= 5.0,
+            Direction::Right => self.x += 5.0,
+        }
+    }
+    pub fn set_direction(&mut self, direction: Direction) {
+        self.direction = direction;
+    }
+
+    pub fn reset(&mut self) {
+        self.x = 100.0;
+        self.y = 100.0;
     }
 }
